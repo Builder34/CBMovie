96812d38714cd246c961080a24cf6362ea084a8a
Builder34 <lcbiao34@gmail.com>
2015-08-07 14:37:38 +0800

    Initial Commit

---
+++ CBMovie/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CBMovie
+//
+//  Created by builder34 on 15/8/7.
+//  Copyright (c) 2015年 builder34. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CBMovie/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CBMovie
+//
+//  Created by builder34 on 15/8/7.
+//  Copyright (c) 2015年 builder34. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
